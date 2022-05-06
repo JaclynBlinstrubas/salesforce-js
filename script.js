1a490e63e1e148d30beccbe8eb82b73331af4073
JBlin1819 <jaclyn.blinstrubas@gmail.com>
2022-05-06 06:22:43 -0400

    callback functions

---
+++ script.js
@@ -0,0 +1,9 @@
+// CALLBACK FUNCTIONS
+
+function doSomething(msg, callback) {
+  setTimeout(function () {
+    console.log(msg)
+    callback()
+  }, 1000)
+}
+doSomething('1st call', function () {})
